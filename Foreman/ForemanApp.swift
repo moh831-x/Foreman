3724db9c861558da1d1176ad3c3b78dded07da21
moh831-x <hossain6944@gmail.com>
2026-08-25 18:24:50 -0400

    Initial Commit

---
+++ Foreman/ForemanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForemanApp.swift
+//  Foreman
+//
+//  Created by Mohammed Hossain on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ForemanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
